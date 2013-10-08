ef8b94af22bb67012ac0d5e9a2dec7ea9f2b72f6
qmark <dimka.qmark@gmail.com>
2013-10-08 21:53:21 +0300

    всчммсч

---
+++ Assets/Controls.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	
+	void start () {	
+		Application.LoadLevel(1);		
+	}
+	
+	
+	void exit () {
+		Application.Quit();
+	}
+	
+}
